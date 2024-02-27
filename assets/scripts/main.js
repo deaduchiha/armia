df257a859fd0b327dc929afbb2c05ced922747ec
mehranyasari <yasarimehran@gmail.com>
2024-02-27 19:56:21 +0330

    build: add armia's project to git

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  cardsEffect: {
+    perSlideOffset: 12, // Space between cards in px
+    perSlideRotate: 0, // Rotation of cards in degrees
+  },
+});
